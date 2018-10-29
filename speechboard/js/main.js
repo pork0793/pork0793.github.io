4a0ddbed321d6bb2921b9c36b498558ff8f5a154
Nato0501 <pork029029@gmail.com>
2018-10-29 21:02:44 +0900

    v1.0.0 [beta]

---
+++ speechboard/js/main.js
@@ -0,0 +1,38 @@
+function dictation() {
+	(function(win, doc) {
+
+		document.getElementById('dic_start').play();
+		document.getElementById('speak_switch').style.display = "none";
+		document.getElementById('msg').textContent = "";
+
+		"use strict";
+
+		var recognition = new webkitSpeechRecognition(),
+		msg         = doc.getElementById("msg");
+
+		recognition.lang = "ja";
+
+		recognition.addEventListener("start", function() {
+
+		});
+
+		recognition.addEventListener("result", function(evt) {
+			var txt = evt.results[0][0].transcript;
+
+			if (txt) {
+				msg.innerText += evt.results[0][0].transcript;
+				msg.innerHTML += "<br />";
+				document.getElementById('dic_stop').play();
+				document.getElementById('speak_switch').style.display = "inline";
+			}
+		}, false);
+
+		recognition.addEventListener("end", function() {
+			document.getElementById('dic_stop').play();
+			document.getElementById('speak_switch').style.display = "inline";
+		});
+
+		recognition.start();
+
+	})(this, document);
+}
